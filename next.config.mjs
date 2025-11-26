b083fc4bf627d04cddc32df643948071827a094c
userAKAANS <besttieshello5@gmail.com>
2025-11-26 19:28:19 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true
-  }
+    serverActions: {},
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
 export default nextConfig;
