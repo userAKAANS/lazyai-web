2b12208390097a7500a003217deb4b64907aa046
userAKAANS <besttieshello5@gmail.com>
2025-11-26 19:42:29 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+    tsconfigPath: null,
+  },
+  webpack: (config) => {
+    // Ignore .ts, .tsx, .d.ts files at build time
+    config.module.rules.push({
+      test: /\.(ts|tsx|d.ts)$/,
+      use: [],
+    });
+    return config;
   },
 };
 
